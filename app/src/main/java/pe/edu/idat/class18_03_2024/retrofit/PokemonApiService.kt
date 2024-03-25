1cd3c60df5058dacf25360c21f75f8b7eba23302
Joseph-MHY <huamaniyallij@gmail.com>
2024-03-25 12:53:05 -0500

    Pokemone cargados

--- app/src/main/java/pe/edu/idat/class18_03_2024/retrofit/PokemonApiService.kt
+++ app/src/main/java/pe/edu/idat/class18_03_2024/retrofit/PokemonApiService.kt
@@ -3,11 +3,12 @@ package pe.edu.idat.class18_03_2024.retrofit
 import pe.edu.idat.class18_03_2024.retrofit.response.PokemonResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PokemonApiService {
 
     @GET("pokemon")
-    fun getPokemon():Call<PokemonResponse>
+    fun getPokemon(@Query("offset")offset:Int, @Query("limit")limit:Int):Call<PokemonResponse>
     /*
     @POST("pokemon")
     fun postPokemon(@Body pokemon: Pokemon)
